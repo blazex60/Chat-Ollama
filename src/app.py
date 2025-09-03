f6771eea6a399d9aebab971d65b06061cf5e977d
blazex60 <ktc25a31k0001@edu.kyoto-tech.ac.jp>
2025-09-03 09:06:11 +0900

    Implement code changes to enhance functionality and improve performance

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from llm import lmstudio as lm
 import re
+"""
+st.set_page_config(
+    page_title="省エネとは無縁の世界", 
+    page_icon=image, 
+)
+"""
+st.title("省エネとは無縁の世界")
+st.icon("../images/icon.png")
 
 # Sidebarの選択肢を定義する
 options = lm.list_loaded_models()
